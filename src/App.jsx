414532d7de7425f845ad7adf101d0f42fb62b106
TSingh's Maccy <testaccount@Mac.lan>
2025-06-14 16:52:20 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import { TokenLaunchpad } from './components/TokenLaunchPad.jsx';
+
+function App() {
+  return (
+    <TokenLaunchpad></TokenLaunchpad>
+  )
+}
+
+export default App
